b1ba11aa5d3075700d863b0d6ff25699ae5afb2d
John Troy Lagahit <johntroylagahit63@gmail.com>
2026-04-13 17:57:33 +0800

    prefinal exma

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>JOHN TROY LAGAHIT, 2nd YEAR 2A</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
+  },
+  text:{
+    color: "white",
+  }
+});
